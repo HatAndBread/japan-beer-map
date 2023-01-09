c7a50cf1d8073bb13b574e8de0554660be10fca4
HatAndBread <61222965+HatAndBread@users.noreply.github.com>
2023-01-09 10:42:58 +0900

    Change map language

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -3,7 +3,16 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = [];
   connect() {
-    const language = new MapboxLanguage();
-    window.map.addControl(language);
+    const mapName = window.map.getStyle().name
+    const language = this.getLanguage();
+    if (mapName === "Mapbox Streets Japan-en" && language !== "en") {
+      window.map.setStyle(window.japaneseURL)
+    } else if (mapName !== "Mapbox Streets Japan-en" && language === "en") {
+      window.map.setStyle(window.englishURL)
+    }
+  }
+
+  getLanguage() {
+    return document.getElementsByName("i18n-lang")[0].dataset.lang;
   }
 }
--- app/models/place.rb
+++ app/models/place.rb
@@ -3,4 +3,6 @@ class Place < ApplicationRecord
   belongs_to :user, optional: true
 
   def owner = user
+
+  def map_data = all.pluck(:lng, :lat, :name, :is_brewery, :is_bar, :is_shop, :is_restaurant)
 end
