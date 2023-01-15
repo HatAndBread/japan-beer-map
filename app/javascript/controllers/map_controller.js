059487588df3f181143b88df7330f0163c4f06e4
Patrick Campbell <campbellpatrickdwight@gmail.com>
2023-01-15 13:29:07 +0900

    boundry goes from an island outside taiwan to a russian island

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -3,8 +3,8 @@ import { distance } from "lib/distance"
 
 const startLngLat = [139.6503, 35.6762]
 const maxBounds = [
-  [123.29314338022773, 24.092580544132286], // Southwest coordinates
-  [151.41816085625925, 46.53952466389388] // Northeast coordinates
+  [121.83031059936349, 20.705762031300967], // Southwest coordinates
+  [154.74125158604622, 49.413834542307185] // Northeast coordinates
   ]
 
 export default class extends Controller {
