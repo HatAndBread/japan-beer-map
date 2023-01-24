2dc81d92210b1337bfd16007860f109e26f32acf
HatAndBread <61222965+HatAndBread@users.noreply.github.com>
2023-01-25 07:37:26 +0900

    Fix accidental overwrite

--- db/migrate/20230108074402_create_visits.rb
+++ db/migrate/20230108074402_create_visits.rb
@@ -1,6 +1,6 @@
 class CreateVisits < ActiveRecord::Migration[7.0]
   def change
-    add_column :place_updates do |t|
+    create_table :visits do |t|
       t.datetime :time
       t.references :user, null: false, foreign_key: true
       t.references :place, null: false, foreign_key: true
