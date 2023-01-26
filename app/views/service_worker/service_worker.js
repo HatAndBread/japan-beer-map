fe646970dd762a3492b851157e6e99b3ecb49cd9
HatAndBread <61222965+HatAndBread@users.noreply.github.com>
2023-01-27 07:34:02 +0900

    Remove logging

--- app/views/service_worker/service_worker.js
+++ app/views/service_worker/service_worker.js
@@ -1,13 +1,10 @@
 function onInstall(event) {
-  console.log('[Serviceworker]', "Installing!", event);
 }
  
 function onActivate(event) {
-  console.log('[Serviceworker]', "Activating!", event);
 }
  
 function onFetch(event) {
-  console.log('[Serviceworker]', "Fetching!", event);
 }
 self.addEventListener('install', onInstall);
 self.addEventListener('activate', onActivate);
