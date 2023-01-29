ecb736a7e028f93f3541c662de9e1bc75bd16ced
HatAndBread <61222965+HatAndBread@users.noreply.github.com>
2023-01-29 18:52:48 +0900

    Add google place gette

--- lib/services/google_importer.rb
+++ lib/services/google_importer.rb
@@ -6,29 +6,45 @@ module Services
     end
 
     def start
-      data.then do |d|
-        d.each do |place|
-          next if place[:business_status] == "CLOSED_PERMANENTLY"
-          next if Place.find_by(google_place_id: place[:place_id])
-          next if Place.where(lng: place[:geometry][:location][:lng], lat: place[:geometry][:location][:lng]).exists?
+      data = get_data
+      data.each_with_index do |place, i|
+        next if place[:business_status] == "CLOSED_PERMANENTLY"
+        next if Place.find_by(google_place_id: place[:place_id])
+        next if Place.where(lng: place[:geometry][:location][:lng], lat: place[:geometry][:location][:lng]).exists?
 
-          place = unsaved_place(place)
-          dupes = possible_duplicates(place)
-          if dupes.any?
-            puts "Possible duplicates!".red
-            ap dupes
-          end
-          ap place
-          print "Do you want to save this place? "
-          answer = gets.chomp!
-          place.save! if answer.downcase == "y"
+        place = place_data(place)
+        reviews = place[:reviews]
+        place = unsaved_place(place)
+        dupes = possible_duplicates(place)
+        puts "*************** Reviews ****************"
+        ap reviews
+        puts "*************** Place ****************"
+        ap place
+        if dupes.any?
+          puts "Possible duplicates!".red
+          ap dupes
+        end
+        puts "Place #{i} out of #{data.size}".cyan
+        puts place.google_maps_url
+        print "Do you want to save this place? "
+        answer = gets.chomp!
+        if answer.downcase == "y"
+          place.save!
+          save_reviews!(reviews, place)
         end
       end
     end
 
     private
 
-    def data
+    def place_data(place)
+      url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=#{place[:place_id]}&key=#{ENV["GOOGLE"]}"
+      result = JSON.parse(RestClient.get(url)).with_indifferent_access[:result]
+      ap result
+      result
+    end
+
+    def get_data
       result = JSON.parse(RestClient.get(@url)).with_indifferent_access[:results]
       ap result
       result
@@ -37,6 +53,20 @@ module Services
     def possible_duplicates(place)
       @places.select do |p|
         p.lng.round(3) == place.lng.round(3) && p.lat.round(3) == place.lng.round(3)
+      end
+    end
+
+    def save_reviews!(reviews, place)
+      reviews.each do |review|
+        attrs = {
+          user: User.where(email: "hello@beermap.jp").first,
+          place:,
+          text: review[:text].delete("\n"),
+          rating: review[:rating],
+          time: Time.at(review[:time].to_i),
+          language: review[:language]
+        }
+        Review.create!(attrs)
       end
     end
 
