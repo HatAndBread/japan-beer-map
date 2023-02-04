6d5a2e3db9d723918763b8bec104b0b6acf6a6f4
HatAndBread <61222965+HatAndBread@users.noreply.github.com>
2023-02-04 10:54:58 +0900

    Fix js translate

--- app/javascript/lib/translate.js
+++ app/javascript/lib/translate.js
@@ -1,5 +1,12 @@
 export const translate = (thing) => {
   const language = document.getElementsByName("i18n-lang")[0].dataset.lang;
-  const translations = JSON.parse(document.getElementsByName("js-translations")[0].dataset.data);
-  return translations[thing][language]
+  return translations()[thing][language]
+}
+function translations() {
+  return {
+    map_error: {
+      en: "There was an error finding your location. Make sure location tracking is enabled in your browser. If you followed a link to this application from inside another application try opening this application directly in your default web browser.",
+      ja: "あなたの位置を見つけるエラーが発生しました。ブラウザの位置トラッキングが有効になっていることを確認してください。別のアプリケーションからこのアプリケーションへのリンクに従った場合は、デフォルトのWebブラウザでこのアプリケーションを直接開いてください。"
+    }
+  }
 }
